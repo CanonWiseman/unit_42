3e5ccd1ead46c13947f7f560fb7bdf29f2526b54
CanonWiseman <wisemancanon@gmail.com>
2023-06-16 00:16:24 -0500

    added unroll array project

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,18 @@
 function unroll(squareArray) {
 
+    let returnArr = [];
+
+    while(squareArray.length > 0){
+        returnArr = [...returnArr, squareArray.shift()];
+
+        returnArr = [...returnArr, squareArray.map(row => row.pop())];
+
+        returnArr = [...returnArr, squareArray.pop().reverse()];
+
+        returnArr = [...returnArr, squareArray.map(row => row.shift()).reverse()];
+    }
+    
+    return returnArr.flat();
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -6,4 +6,32 @@ describe("#unroll", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  test("it works for square arrays", () =>{
+    let squareArray = [
+      [1,2,3,4],
+      [5,6,7,8],
+      [9,10,11,12],
+      [13,14,15,16]
+    ]
+
+    let res = unroll(squareArray);
+    expect(res).toEqual(
+      [1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]
+    );
+  });
+
+  test("it works with uneven arrays", () =>{
+    let unevenArray = [
+      [1,2,3,4],
+      [5,6,7,8,8.5],
+      [9,10,11,12],
+      [13,14,15,16,17]
+    ]
+
+    let res = unroll(unevenArray);
+    expect(res).toEqual(
+      [1,2,3,4,8.5,12,17,16,15,14,13,9,5,6,7,8,11,10]
+    )
+  });
+
 });
